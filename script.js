467c4ef14c1f258039660639ac32c4848d312073
painter98 <120320713+painter98@users.noreply.github.com>
2023-04-26 10:29:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function load() {
+  var text = "";
+  if (navigator.cookieEnabled == true) {
+    text = "DOM load success";
+  } 
+  document.getElementById("demo").innerHTML = text;
+}
